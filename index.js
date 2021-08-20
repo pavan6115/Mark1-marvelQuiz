b66d3c2a6297f7895df14a20aeb65225e51f0f20
Pavan Kulkarni <55844228+pavan6115@users.noreply.github.com>
2021-08-20 13:22:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const readlineSync = require('readline-sync')
+
+let quizScore = 0
+console.log('= = = = = = Welcome to Quiz = = = = = =')
+const userName = readlineSync.question('What is your name? ')
+console.log('Welcome, ' + userName + '.')
+
+function quiz(question, answer) {
+    const userAns = readlineSync.question(question)
+    if (userAns === answer) {
+        console.log('You have answered right!')
+        quizScore += 2
+    }
+    else {
+        console.log('You have answered wrong!')
+        quizScore -= 1
+    }
+    console.log('Your score is -> ' + quizScore)
+    console.log('- - - - - - - - - - -')
+}
+
+// Array of questions
+const questions = [
+    {
+        question : 'Where does Iron Man live? ',
+        answer : 'Midtown USA'
+    },
+    {
+        question : 'What is special about Captain America? ',
+        answer : 'Shield'
+    },
+    {
+        question : 'Is Flash a part of Marvel? ',
+        answer : 'No'
+    },
+    {
+        question : 'Have you watched Loki yet? ',
+        answer : 'Yes'
+    }
+]
+
+// for loop
+for (let que in questions){
+    quiz(questions[que].question, questions[que].answer)
+}
+
+console.log(userName + ' you have scored -> ' + quizScore)
